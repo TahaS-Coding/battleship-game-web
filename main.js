24eb65bc6480ba2d8548176e9dbad9b254180f4b
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-04-03 14:51:12 -0700

    Began creating ship hit detection
    Me and Jun worked together to come up with a system for ship coordinates that are able to change.

--- main.js
+++ main.js
@@ -1,16 +1,92 @@
 document.addEventListener(`DOMContentLoaded`, function() {
-    // OVERLAY BOARD
     let gameBoardOverlay = document.getElementById('gameBoardOverlay');
+    let gameBoard = new Map();
+    // 2 for loops to create a 2d array for the OVERLAY board as well as create a map for the actual GAME baord
     for (rows = 1; rows < 21; rows++) {
         for (columns = 1; columns < 21; columns++) {
+            // Creates button elements with unique coordinate id's as children of overlay
             let sqrID = `x${columns}y${rows}`;
             let newSquare = document.createElement("button");
             newSquare.setAttribute("id", sqrID);
             newSquare.classList.add("squares");
             newSquare.addEventListener("click", function(){
-                console.log(this.id);
+                for(let ship of shipList){
+                    if(this.getAttribute("id") == `x${ship.xCoord}y${ship.yCoord}`){
+                        // if (ship.rotation == "horizontal"){
+                        //     for (let i = 0; i < ship.xCoord; i++){
+                                
+                        //     }
+                        //     for (let i = 0; i < ship.yCoord; i++){
+                                
+                        //     }
+                        // }
+                        console.log("Hit SHip");
+                    }
+                    else{
+                        console.log("No ship has been hit");
+                    }
+                }
             })
-            gameBoardOverlay.appendChild(newSquare);					
+            gameBoardOverlay.appendChild(newSquare);
+            //creates a map for gameboard squares 
+            gameBoard.set(`${sqrID}`, {});			
         }
     }
+
+    class AircraftCarrier{
+        constructor(){
+            this.xSize = 5;
+            this.ySize = 2;
+            this.xCoord = 2;
+            this.yCoord = 2;
+            this.rotation = "horizontal";
+        }
+
+        
+    }
+    let shipList = [new AircraftCarrier()]
+    class Destroyer{
+        constructor(){
+            this.xSize = 4;
+            this.ySize = 1;
+        }
+
+    }
+    class Cruiser{
+        constructor(){
+            this.xSize = 2;
+            this.ySize = 1;
+        }
+
+    }
+    class Battleship{
+        constructor(){
+            this.xSize = 4;
+            this.ySize = 1;
+        }
+
+    }
+    class Submarine{
+        constructor(){
+            this.xSize = 3;
+            this.ySize = 1;
+        }
+
+    }
 });
+
+function ShipRNG() {
+    let shipCoord = [];
+    // Keeps track of coordinate that have already been taken
+    let random = (Math.floor(Math.random()*100));
+    if(random > 20) {
+        random - 20
+    } else {
+        console.log(random) 
+    }
+    console.log(random)
+}
+
+
+
+ShipRNG()
