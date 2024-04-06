c3d94320ee5a105ce46d7b9323ed34c089efdd9f
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-04-05 22:15:16 -0700

    Created working ship hit detection
    When clicking on the overlay board, one is able to detect a hit on a ship for all squares that it is covering

--- main.js
+++ main.js
@@ -4,47 +4,43 @@ document.addEventListener(`DOMContentLoaded`, function() {
     // 2 for loops to create a 2d array for the OVERLAY board as well as create a map for the actual GAME baord
     for (rows = 1; rows < 21; rows++) {
         for (columns = 1; columns < 21; columns++) {
-            // Creates button elements with unique coordinate id's as children of overlay
+            // Creates button elements with unique coordinate id's as children of overlay, also have ship hit detection on click
             let sqrID = `x${columns}y${rows}`;
             let newSquare = document.createElement("button");
             newSquare.setAttribute("id", sqrID);
             newSquare.classList.add("squares");
-              // detects if square has a ship on it
             newSquare.addEventListener("click", function(){
                 // gets coordinates of clicked square
                 let clickedSqrID = this.getAttribute("id");
-                let splitID = this.getAttribute("id").split('');
+                let splitID = clickedSqrID.match(/\d+/g);
                 let sqrXCoord = null;
                 let sqrYCoord = null;
+                console.log(splitID);
                 for (let char of splitID){
-                    if (typeof char == "number" && sqrXCoord == null){
-                        sqrXCoord = Number(char);
+                    let num = Number(char);
+                    if (sqrXCoord == null){
+                        sqrXCoord = num;
                     }
-                    else if(typeof char == "number" && sqrXCoord != null){
-                        sqrYCoord = Number(char);
+                    else if(sqrXCoord != null){
+                        sqrYCoord = num;
                     }
                 }
+                console.log(sqrXCoord);
+                console.log(sqrYCoord);
                 // sees if ship is on that coordinate
                 for(let ship of shipList){
-                    if(ship.rotation == "horizontal"){
-                        for(let i = sqrXCoord; i > sqrXCoord - ship.size; i--){
-                            if(clickedSqrID == `x${i}y${ship.yCoord}`){
-                                console.log("Hit ship");
-                            }
+                    if(ship.rotation == "horizontal" && ship.yCoord == sqrYCoord){
+                        if (sqrXCoord <= ship.xCoord && sqrXCoord > ship.xCoord - ship.size){
+                            console.log("hit carrier");
                         }
                     }
-                    else if(ship.rotation == "vertical"){
-                        for(let i = sqrYCoord; i < sqrYCoord + ship.size; i++){
-                            if(clickedSqrID == `x${ship.xCoord}y${i}`){
-                                console.log("Hit ship");
-                            }
+                    else if(ship.rotation == "vertical" && ship.xCoord == sqrXCoord){
+                        if (sqrYCoord >= ship.yCoord && sqrYCoord < ship.yCoord + ship.size){
+                            console.log("hit destroyer");
                         }
-                    }
-                    else{
-                        console.log("No ship hit");
                     }
                 }
-            })
+            });
             gameBoardOverlay.appendChild(newSquare);
             //creates a map for gameboard squares 
             gameBoard.set(`${sqrID}`, {});			
