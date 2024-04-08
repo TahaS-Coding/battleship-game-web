83911c26b66836e567ec35425ca26b2714fd10bc
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-04-08 15:49:57 -0700

    Working on random coord gen
    Finished a random coordinate generator and are now working to make sure ships dont overlap

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
-document.addEventListener(`DOMContentLoaded`, function() {
+document.addEventListener(`DOMContentLoaded`, function () {
     let gameBoardOverlay = document.getElementById('gameBoardOverlay');
     let gameBoard = new Map();
     // 2 for loops to create a 2d array for the OVERLAY board as well as create a map for the actual GAME baord
-    for (rows = 1; rows < 21; rows++) {
+    for (rows = 20; rows > 0; rows--) {
         for (columns = 1; columns < 21; columns++) {
             // Creates button elements with unique coordinate id's as children of overlay, also have ship hit detection on click
             let sqrID = `x${columns}y${rows}`;
             let newSquare = document.createElement("button");
             newSquare.setAttribute("id", sqrID);
             newSquare.classList.add("squares");
-            newSquare.addEventListener("click", function(){
+            newSquare.addEventListener("click", function () {
                 // gets coordinates of clicked square
                 let clickedSqrID = this.getAttribute("id");
                 let splitID = clickedSqrID.match(/\d+/g);
                 let sqrXCoord = null;
                 let sqrYCoord = null;
-                for (let char of splitID){
+                for (let char of splitID) {
                     let num = Number(char);
-                    if (sqrXCoord == null){
+                    if (sqrXCoord == null) {
                         sqrXCoord = num;
                     }
-                    else if(sqrXCoord != null){
+                    else if (sqrXCoord != null) {
                         sqrYCoord = num;
                     }
                 }
+                // Shows coord in console.log() when hovering squares
+                newSquare.addEventListener("mouseover", function(){
+                    let spanCoord = newSquare.getAttribute("id");
+                    console.log(spanCoord)
+                })
+
                 // sees if ship is on that coordinate
-                for(let ship of shipList){
-                    if(ship.rotation == "horizontal" && ship.yCoord == sqrYCoord){
-                        if (sqrXCoord <= ship.xCoord && sqrXCoord > ship.xCoord - ship.size){
+                for (let ship of shipList) {
+                    if (ship.rotation == "horizontal" && ship.yCoord == sqrYCoord) {
+                        if (sqrXCoord <= ship.xCoord && sqrXCoord > ship.xCoord - ship.size) {
                             alert(`hit carrier at (${clickedSqrID})`);
                         }
                     }
-                    else if(ship.rotation == "vertical" && ship.xCoord == sqrXCoord){
-                        if (sqrYCoord >= ship.yCoord && sqrYCoord < ship.yCoord + ship.size){
+                    else if (ship.rotation == "vertical" && ship.xCoord == sqrXCoord) {
+                        if (sqrYCoord <= ship.yCoord && sqrYCoord > ship.yCoord - ship.size) {
                             alert(`hit destroyer at (${clickedSqrID})`);
                         }
                     }
@@ -40,52 +46,73 @@ document.addEventListener(`DOMContentLoaded`, function() {
             });
             gameBoardOverlay.appendChild(newSquare);
             //creates a map for gameboard squares 
-            gameBoard.set(`${sqrID}`, {});			
+            gameBoard.set(`${sqrID}`, {});
         }
     }
 
-    class AircraftCarrier{
-        constructor(){
-            this.name = "aircraftcarrier";
+    class AircraftCarrier {
+        constructor() {
+            this.name = "AircraftCarrier";
             this.size = 5;
-            this.xCoord = 8;
-            this.yCoord = 10;
+            this.xCoord = undefined;
+            this.yCoord = undefined;
             this.rotation = "horizontal";
+            this.occupiedSquares = undefined;
         }
     }
-    class Destroyer{
-        constructor(){
-            this.name = "destroyer";
+    class Destroyer {
+        constructor() {
+            this.name = "Destroyer";
             this.size = 4;
-            this.xCoord = 15;
-            this.yCoord = 15;
+            this.xCoord = undefined;
+            this.yCoord = undefined;
             this.rotation = "vertical";
+            this.occupiedSquares = undefined;
         }
     }
     let shipList = [new AircraftCarrier(), new Destroyer()];
 
-    function rNG(){
-        return Math.floor((Math.random() * 20) + 1);
-    }
-    
-    for (let ship of shipList) {
-        if (ship.rotation == "horizontal") {
-            ship.xCoord = rNG();
-            while (ship.xCoord < ship.size) {
-                ship.xCoord = rNG();
-            }
-            ship.yCoord = rNG();
-        }
-        else if(ship.rotation == "vertical"){
-            ship.yCoord = rNG();
-            while (ship.yCoord > 20 - ship.size){
-                ship.yCoord = rNG();
-            }
-            ship.xCoord = rNG();
-        }
-    }
 
-    for (let ship of shipList){
+
+    function rng() {
+        let rng = Math.floor((Math.random() * 20) + 1);
+        // Generates a number 1 - 20  
+        // The 20 makes it generate a number less than 20
+        // It actually generating a 0 - 19, so adding 1 solves the problem
+        return (rng);
+    }
+    function randomCoords(inputShip){
+        if (inputShip.rotation == "horizontal") {
+            inputShip.xCoord = rng();
+            while (inputShip.xCoord < inputShip.size) {
+                inputShip.xCoord = rng();
+            }
+            inputShip.yCoord = rng();
+        }
+        else if (inputShip.rotation == "vertical") {
+            inputShip.yCoord = rng();
+            while (inputShip.yCoord < inputShip.size) {
+                inputShip.yCoord = rng();
+            }
+            inputShip.xCoord = rng();
+        }
+    }
+    for (let ship of shipList) {
+        randomCoords(ship);
+        if(ship.rotation)
+        ship.occupiedSquares = [[ship.xCoord, ship.yCoord]];
+        for (let i = 0; i < shipList.length; i++){
+            let shipCoordinate = shipList[i].occupiedSquares;
+            if (ship.occupiedSquares[0] == shipCoordinate[0] && ship.occupiedSquares[1] == shipCoordinate[1]){
+                randomCoords(ship);
+                i = 0;
+            }
+        }
+    }
+    for (let ship of shipList) {
         console.log(`the ${ship.name}'s coordinates are (${ship.xCoord}, ${ship.yCoord}) `)
     }
+
+
+    
 });
--- randomize.js
+++ randomize.js
@@ -1,54 +1,64 @@
-
+// Random number generator
 function rng() {
-    let rng = Math.floor(Math.random() * 100);
-    // Generates a number 0 - 99
-    do {
-        rng += rng;
-    } while (rng <= 0)
-    // This loops runs until rng is not a zero
-    // We want to generate a number from 1 - 20
-
-
-    // This is a loops that subtracts 20 if the number is over 20
-    // The loop will stop if it is 20 or less
-    if (rng > 20) {
-        do {
-            rng = rng - 20;
-        } while (rng > 20);
-
-
-        return (rng);
-    }
-    else {
-        return (rng);
-    }
+    let rng = Math.floor((Math.random() * 20) + 1);
+    // Generates a number 1 - 20  
+    // The 20 makes it generate a number less than 20
+    // It actually generating a 0 - 19, so adding 1 solves the problem
+    return (rng);
 }
-// console.log(rng())
 
 
+let shipCoordinate = [];
+// Keeps track of coordinate that have already been taken
+// Use this to check if ship is hit
+// Maybe we are going to need two for each player
+let shipHit = [];
+// Keeps track ships hit
 
+
+// This is going to be used to create new ship
 class Ship {
-    constructor(health,length,d,coord, newCoord) {
+    constructor(name, health, length, speed) {
+        this.name = name;
         this.health = health;
         this.length = length;
-        this.direction = d;
-        this.oldCoord = coord;
-        this.newCoord = newCoord;
-    }
-
-    get long() {
-        return 1;
+        this.movementSpeed = speed;
+        this.direction = generateDirection();
+        this.orientation = ori(this.direction);
+        this.coordinate = [];
     }
 }
 
+
+
+
+
+
+// Create new following the instruction
+// name, health, length, movement speed
+let battleship = new Ship(`Battleship`, 8, 8, 3);
+let cruiser = new Ship(`Cruiser`, 5, 6, 5);
+// console.log(battleship, cruiser)
+let shipLists = [battleship, cruiser]
+
+
+
+
+// console.log(battleship.health -= 1)
+// console.log(battleship)
+
+
+
+
 // This function uses the number generator to decide what direction the ships is going
-function direction() {
+// Going to need to check if direction is available
+function generateDirection() {
     let d = rng();
     if (d <= 5) {
         return `up`;
     } else if (d <= 10) {
         return `down`;
-    } else if (d <=15) {
+    } else if (d <= 15) {
         return `left`;
     } else {
         return `right`;
@@ -56,26 +66,139 @@ function direction() {
 }
 
 
-// Avoid repeating by using class
-// health, length, movement speed, directon, old coordinate, new coordinate 
-let battleship = new Ship(5,5,direction(),coordinate(5),hey());
+// Determines the ship orientation
+// up and down = vertical
+// left and right = horizontal
+// I forgot why I made this
+function ori(d) {
+    switch (d) {
+        case (`left`):
+            // console.log(`horizontal`)
+            return `horizontal`;
+        case (`right`):
+            // console.log(`horizontal`)
+            return `horizontal`;
+        case (`up`):
+            // console.log(`vertical`) 
+            return `vertical`;
+        case (`down`):
+            // console.log(`vertical`)
+            return `vertical`;
+    }
+}
 
 
-console.log(battleship)
-console.log(battleship.oldCoord)
+// Add x or y
+function Add(name, length, axis, xy) {
+    let i = 0;
+    do {
+        i++;
+        axis++;
+        let result = `x${axis}y${xy}`;
+        shipCoordinate.push(result);
+        name.coordinate.push(result)
+    } while (i < length);
+}
+// Subtract x or y  
+function Sub(name, length, axis, xy) {
+    let i = 0;
+    do {
+        i++;
+        axis--;
+        let result = `x${axis}y${xy}`;
+        shipCoordinate.push(result);
+        name.coordinate.push(result)
+    } while (i < length);
+}
+console.log(shipCoordinate)
 
 
-function coordinate(length) {
-   
-    let l = length;
-    console.log(l)
-    // Length of ship
+function moveAxis(name, dir, length, x, y) {
+    
+    switch (dir) {
+        case (`left`):
+            Sub(name, length, x, y)
+            break;
+        case (`right`):
+            Add(name, length, x, y)
+            break;
+        case (`up`):
+            Add(name, length, y, x)
+            break;
+        case (`down`):
+            Sub(name, length, y, x)
+            break;
+    }
+
+
+}
+
+
+
+
+function generateCoordinate(n, c, l, d) {
+    // n is ship name
+    // c is the coordinate
+    // l is the length of the ship
+    // d is the direction
+
+
     let x = rng()
     let y = rng()
-    return [`x` + x + `y` + y];
-   
+    moveAxis(n, d, l, x, y)
+    let result = `x${x}y${y}`;
+    c.push(result)
+    shipCoordinate.push(result);
+    // console.log(result)
+    return result;
+
+
 }
 
-function hey(){
-    return `hi`
+
+
+
+// let bc = (battleship.coordinate)
+// bc.push(generateCoordinate())
+
+
+
+
+// console.log(battleship.coordinate)
+function coordinate() {
+    shipLists.forEach((element) => {
+        let length = element.length;
+        let direction = element.direction;
+        let coordinate = element.coordinate;
+
+
+        if (coordinate.length === 0) {
+            generateCoordinate(element, coordinate, length, direction)
+            // console.log(shipCoordinate)
+        }
+        coordinate.forEach((element) => {
+            let result = shipCoordinate.indexOf(element);
+            shipHit.push(result);
+            // console.log(result)
+        })
+        // console.log(shipHit, length);
+
+
+    });
 }
+coordinate()
+// this function is going to be called at the start of the game and every round
+// the direction function is also going to be called every round
+
+
+
+
+// how am I going to have these code run for each player
+// we are going to figure this out later, first get these code to work
+// grab old coord and create new coord
+// use direction() to choose direction of movement
+// check if coord is valid, if not get new direction
+// randomize the amount of square moved  
+
+
+
