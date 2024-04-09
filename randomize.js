4b91fadd2e8686137af38ca174be61662b0f924e
IlikeCookieJK <jun_hong.kuang23@student.cart.org>
2024-04-09 14:59:52 -0700

    Worked on coordinates
    Not finished

--- randomize.js
+++ randomize.js
@@ -8,12 +8,16 @@ function rng() {
 }
 
 
+
+
 let shipCoordinate = [];
 // Keeps track of coordinate that have already been taken
 // Use this to check if ship is hit
 // Maybe we are going to need two for each player
 let shipHit = [];
 // Keeps track ships hit
+
+
 
 
 // This is going to be used to create new ship
@@ -34,6 +38,12 @@ class Ship {
 
 
 
+
+
+
+
+
+
 // Create new following the instruction
 // name, health, length, movement speed
 let battleship = new Ship(`Battleship`, 8, 8, 3);
@@ -44,8 +54,16 @@ let shipLists = [battleship, cruiser]
 
 
 
+
+
+
+
 // console.log(battleship.health -= 1)
 // console.log(battleship)
+
+
+
+
 
 
 
@@ -66,6 +84,8 @@ function generateDirection() {
 }
 
 
+
+
 // Determines the ship orientation
 // up and down = vertical
 // left and right = horizontal
@@ -79,7 +99,7 @@ function ori(d) {
             // console.log(`horizontal`)
             return `horizontal`;
         case (`up`):
-            // console.log(`vertical`) 
+            // console.log(`vertical`)
             return `vertical`;
         case (`down`):
             // console.log(`vertical`)
@@ -88,50 +108,61 @@ function ori(d) {
 }
 
 
+
+
 // Add x or y
-function Add(name, length, axis, xy) {
-    let i = 0;
-    do {
-        i++;
-        axis++;
-        let result = `x${axis}y${xy}`;
-        shipCoordinate.push(result);
-        name.coordinate.push(result)
-    } while (i < length);
-}
-// Subtract x or y  
-function Sub(name, length, axis, xy) {
-    let i = 0;
-    do {
-        i++;
-        axis--;
-        let result = `x${axis}y${xy}`;
-        shipCoordinate.push(result);
-        name.coordinate.push(result)
-    } while (i < length);
-}
-console.log(shipCoordinate)
+// function Add(name, length, axis, xy) {
+//     let i = 0;
+//     do {
+//         i++;
+//         axis++;
+//         let result = `x${axis}y${xy}`;
+//         shipCoordinate.push(result);
+//         name.coordinate.push(result)
+//     } while (i < length);
+// }
+// // Subtract x or y  
+// function Sub(name, length, axis, xy) {
+//     let i = 0;
+//     do {
+//         i++;
+//         axis--;
+//         let result = `x${axis}y${xy}`;
+//         shipCoordinate.push(result);
+//         name.coordinate.push(result)
+//     } while (i < length);
+// }
+// console.log(shipCoordinate)
+
+
 
 
 function moveAxis(name, dir, length, x, y) {
-    
+
+
     switch (dir) {
         case (`left`):
-            Sub(name, length, x, y)
+            // Sub(name, length, x, y)
             break;
         case (`right`):
-            Add(name, length, x, y)
+            // Add(name, length, x, y)
             break;
         case (`up`):
-            Add(name, length, y, x)
+            // Add(name, length, y, x)
             break;
         case (`down`):
-            Sub(name, length, y, x)
+            // Sub(name, length, y, x)
             break;
     }
 
 
+
+
 }
+
+
+
+
 
 
 
@@ -141,6 +172,8 @@ function generateCoordinate(n, c, l, d) {
     // c is the coordinate
     // l is the length of the ship
     // d is the direction
+
+
 
 
     let x = rng()
@@ -153,7 +186,13 @@ function generateCoordinate(n, c, l, d) {
     return result;
 
 
+
+
 }
+
+
+
+
 
 
 
@@ -164,12 +203,18 @@ function generateCoordinate(n, c, l, d) {
 
 
 
+
+
+
+
 // console.log(battleship.coordinate)
 function coordinate() {
     shipLists.forEach((element) => {
         let length = element.length;
         let direction = element.direction;
         let coordinate = element.coordinate;
+
+
 
 
         if (coordinate.length === 0) {
@@ -184,11 +229,17 @@ function coordinate() {
         // console.log(shipHit, length);
 
 
+
+
     });
 }
 coordinate()
 // this function is going to be called at the start of the game and every round
 // the direction function is also going to be called every round
+
+
+
+
 
 
 
@@ -199,6 +250,112 @@ coordinate()
 // use direction() to choose direction of movement
 // check if coord is valid, if not get new direction
 // randomize the amount of square moved  
+
+
+class AircraftCarrier {
+    constructor() {
+        this.name = "AircraftCarrier";
+        this.size = 5;
+        this.xCoord = 12;
+        this.yCoord = 13;
+        // this.direction = generateDirection();
+        this.rotation = `horizontal`;
+        this.occupiedSquares = undefined;
+    }
+}
+class Destroyer {
+    constructor() {
+        this.name = "Destroyer";
+        this.size = 4;
+        this.xCoord = 11;
+        this.yCoord = 15;
+        // this.direction = generateDirection();
+        this.rotation = `vertical`;
+        this.occupiedSquares = undefined;
+    }
+}
+let shipList = [new AircraftCarrier(), new Destroyer()];
+
+
+function checkCoord() {
+    let notAvailable = [];
+    for (let ship of shipList) {
+        // randomCoords(ship)
+        let checking = [];
+        let name = ship.name;
+        let xAxis = ship.xCoord;
+        let yAxis = ship.yCoord;
+        let size = ship.size;
+        let rotation = ship.rotation;
+        if (rotation == `horizontal`) {
+            let coordinateList = Sub(size, xAxis, yAxis, `x`);
+            for (let coord of coordinateList) {
+                checking.push(coord);
+            }
+        } else if (rotation == `vertical`) {
+            let coordinateList = Sub(size, yAxis, xAxis, `y`);
+            for (let coord of coordinateList) {
+                checking.push(coord);
+            }
+        }
+        console.log(name, xAxis, yAxis, rotation, size)
+        console.log(checking)
+        let checked = `undefined`;
+        for (let coord of checking) {
+            let c = notAvailable.indexOf(coord)
+            if (c > 0) {
+                checked = `Not Available`;
+                console.log(`Not Available`)
+            } else {
+                checked = `true`;
+            }
+        }
+        if (checked === `true`) {
+            for (let coord of checking) { 
+                notAvailable.push(coord);
+            }
+        } else if(checked === `Not Available`){
+            notAvailable.push(`hi`)
+        }
+
+    }
+    console.log(notAvailable)
+}
+checkCoord()
+function randomCoords(inputShip) {
+    if (inputShip.rotation == "horizontal") {
+        inputShip.xCoord = rng();
+        while (inputShip.xCoord < inputShip.size) {
+            inputShip.xCoord = rng();
+        }
+        inputShip.yCoord = rng();
+    }
+    else if (inputShip.rotation == "vertical") {
+        inputShip.yCoord = rng();
+        while (inputShip.yCoord < inputShip.size) {
+            inputShip.yCoord = rng();
+        }
+        inputShip.xCoord = rng();
+    }
+}
+
+// Subtract x or y  
+function Sub(length, axis, otherAxis, xy) {
+    axis++;
+    // This axis++ makes sure that it starts at the right axis
+    let i = 0;
+    let result = [];
+    while (i < length) {
+        i++;
+        axis--;
+        if (xy == `x`) {
+            result.push(`x${axis}y${otherAxis}`);
+        } else if (xy == `y`) {
+            result.push(`x${otherAxis}y${axis}`);
+        }
+    };
+    return result;
+}
 
 
 
